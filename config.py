06edbbb8f307ac0029278c3984ea0090085a2a31
bcskda <demulkup@ya.ru>
2019-06-12 18:23:39 +0300

    Иконки, оформление

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 path_csv_actor = 'Data/actors.csv'
 path_csv_movie = 'Data/movies.csv'
 
-color_bg = '#d7d8e0'
+color_bg = '#66b3ff'
 
 paths_icons = {
-    'add': "Graphics/add.png",
-    'delete': "Graphics/delete.png",
-    'edit': "Graphics/edit.png",
-    'save': "Graphics/save.png",
-    'plot_view': "Graphics/plot_view.png",
-    'search': "Graphics/search.png"
+    'add': "Graph/add.png",
+    'delete': "Graph/del.png",
+    'edit': "Graph/rewrite.png",
+    'save': "Graph/save.png",
+    'plot_view': "Graph/graph.png",
+    'search': "Graph/search.png"
 }
--- validate.py
+++
@@ -1,7 +0,0 @@
-def parse_year(year: str) -> int:
-    if year is None or year == '':
-        raise ValueError
-    value = int(year)
-    if value < 0:
-        raise ValueError
-    return value
